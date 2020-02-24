5c1e720ccca7ace7d3e327dd2da027591926a666
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-02-24 07:32:42 -0800

    Update List.cpp

--- List.cpp
+++ List.cpp
@@ -5,38 +5,113 @@
 
 using namespace std;
 
-List::List(Node* newHead) {
-
+List::List(){
+    head = NULL;
 }
 
-Node* List::qPop() {
-
+void List::push(char* newValue){
+    pushEnd(head,newValue,NULL,true);
+    return;
 }
 
-Node* List::sPop() {
-
+void List::pushNode(Node* newValue){
+    pushEnd(head,NULL,newValue,false);
 }
 
-Node* List::qPeek() {
-
+char* List::qPop(){
+    if (head == NULL){
+        return NULL;
+    }
+    char* value = copy(head->getValue());
+    Node* temp = head->getNext();
+    delete head;
+    head = temp;
+    return value;
 }
 
-Node* List::sPeek() {
-
+char* List::sPop(){
+    return getEnd(head,true);
 }
 
-Node* List::qPush() {
-
+char* List::qPeek(){
+    if (head == NULL){
+        return NULL;
+    }
+    return head->getValue();
 }
 
-Node* List::sPush() {
-
+char* List::sPeek(){
+    return getEnd(head,false);
 }
 
-Node* List::enqueue() {
-
+Node* List::peekNode(){
+    return getEndNode(head);
 }
 
-Node* List::dequeue() {
+char* List::getEnd(Node* &current,bool de){
+    if (current = NULL){
+        return NULL;
+    }
+    if (current->getNext() == NULL){
+        char* value = copy(current->getValue());
+        if (de){
+            current = NULL;
+            delete current;
+        }
+        return value;
+    }
+    if (current->getNext()->getNext() == NULL){
+        char* value = copy(current->getNext()->getValue());
+        if (de){
+            current->setNext(NULL);
+            delete current->getNext();
+        }
+        return value;
+    }
+    Node* next = current->getNext();
+    getEnd(next,de);
+}
 
+Node* List::getEndNode(Node* &current){
+    if (current == NULL){
+        retun NULL;
+    }
+    else if (current->getNext() == NULL){
+        return current;
+    }
+    Node* next = current->getNext();
+    getEndNode(next);
+}
+
+void List::pushEnd(Node* &current, char* charIn, Node* nodeIn, bool type){
+    if (current == NULL){
+        if (type){
+            current = new Node(charIn);
+        }
+        else{
+            current = nodeIn;
+        }
+        return;
+    }
+    if (current->getNext() == NULL){
+        if (type){
+            current->setNext(new Node(charIn));
+        }
+        else{
+            current->setNext(nodeIn);
+        }
+        return;
+    }
+    Node* next = current->getNext();
+    pushEnd(next,charIn,nodeIn,type);
+}
+
+char* List::copy(char* in){
+    char* out = new char[strlen(in)+1];
+    strcpy(out,in);
+    return out;
+}
+
+List::~List(){
+    delete head;
 }
